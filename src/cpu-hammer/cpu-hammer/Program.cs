09314d2f6b981b4aa6b5a62b8f23ff8f644259bb
Carlos Garcia Lalicata <crgarcia@live.com.ar>
2025-03-23 23:24:47 +0100

    adding cpuhammer

---
+++ src/cpu-hammer/cpu-hammer/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Numerics;
+
+Console.WriteLine("Starting");
+
+static BigInteger CalculatePi(int digits)
+{
+    BigInteger C = 426880 * BigInteger.Pow(10005, 1 / 2);
+    BigInteger K = 6;
+    BigInteger M = 1;
+    BigInteger X = 1;
+    BigInteger L = 13591409;
+    BigInteger S = L;
+
+    for (int i = 1; i < digits; i++)
+    {
+        M = (K * K * K - 16 * K) * M / (i * i * i);
+        L += 545140134;
+        X *= -262537412640768000;
+        S += M * L / X;
+        K += 12;
+    }
+
+    return C / S;
+}
+
+CalculatePi(10)
